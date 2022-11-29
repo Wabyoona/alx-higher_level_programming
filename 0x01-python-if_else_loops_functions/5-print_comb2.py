e901684d8923d28dc3786674236ba85515256aa5
Wabyoona <maxwell@gmail.com>
2022-11-29 12:52:59 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(i)
-elif i < 99:
-    print('{:02d}, '.format(i), end='')
+
+for i in range(0, 99):
+
+        print("{:02d}".format(i), end=", ")
+
+        print(i + 1)
