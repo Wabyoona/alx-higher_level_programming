8cd84036708b97bee005df743d1fb130d80fcc56
Wabyoona <maxwell@gmail.com>
2022-11-29 13:29:57 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in reversed(range(97, 123)):
-if (i % 2 == 0):
-print('{:c}'.format(i), end='')
-else:
-print('{:c}'.format(i - 32), end='')
+
+for ch in reversed(range(97, 123)):
+
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
