a44b8ebb6b35947a3fcba0e074921d4ffab8badd
Wabyoona <maxwell@gmail.com>
2023-03-13 18:38:59 -0700

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,14 +4,14 @@
 const myObject = {
   type: 'object',
   value: 12
-};
-console.log(myObject);
+}
+console.log(myObject)
 myObject.incr = function () {
-  myObject.value++;
-};
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+  myObject.value++
+}
+myObject.incr()
+console.log(myObject)
+myObject.incr()
+console.log(myObject)
+myObject.incr()
+console.log(myObject)
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,12 @@
 // prints 3 lines: (like 1-multi_languages.js) but by
 // using an array of string and a loop
 
-const lang = 'C is fun';
+const lang = 'C is fun'
 
 if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences')
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log(lang);
+    console.log(lang)
   }
 }
